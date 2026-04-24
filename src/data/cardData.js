6647675589b11db40cab527e57bc4e7088d3c0f3
Sasha <st087955@student.spbu.ru>
2026-04-24 20:09:06 +0300

    Создан моковый  массив объектов для карточек

---
+++ src/data/cardData.js
@@ -0,0 +1,6 @@
+export const cardsData = [
+  { id: 1, image: '🐶', isClicked: false },
+  { id: 2, image: '🐱', isClicked: false },
+  { id: 3, image: '🐭', isClicked: false },
+  { id: 4, image: '🐹', isClicked: false }
+];
