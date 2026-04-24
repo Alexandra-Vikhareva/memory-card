cdfd8f48ed4eb8c5d51c4da91a754935606fa0f6
Sasha <st087955@student.spbu.ru>
2026-04-24 22:40:00 +0300

    Логика игры перенесена в отдельный компонент Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
-import { useState, useEffect } from 'react'
 import './App.css'
-import Card from './components/Card'
-import { cardsData } from './data/cardData'
+import Game from './components/Game'
 
 function App() {
-  const [cards, setCards] = useState(cardsData)
-
-  useEffect(() => {
-      console.log(cards);
-    }, [cards]);
-
-  function handleCardClick(id) {
-    let selectedCard = cards.find(item => 
-                            item.id === id )
-
-    if (selectedCard.isClicked) {
-      console.log('Стоп игра')
-    }
-    else {
-      setCards(prev => 
-                (prev.map(item => 
-                  item === selectedCard 
-                  ? {...item, isClicked: true} 
-                  : item)))
-    }
-  }
-
   return (
-    <div className='cardsGrid'>
-      {
-        cards.map((item)=> (
-          <Card image={item.image} 
-                id={item.id}
-                key={item.id}
-                onClick={handleCardClick}></Card>
-        ))   
-      }
-    </div>
+    <Game></Game>
   )
 }
 
---
+++ src/components/Game.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import Card from './Card'
+import { cardsData } from '../data/cardData'
+import '../styles/Game.css'
+
+export default function Game () {
+    const [cards, setCards] = useState(cardsData)
+
+    useEffect(() => {
+        console.log(cards);
+    }, [cards]);
+
+    function handleCardClick(id) {
+    let selectedCard = cards.find(item => 
+                            item.id === id )
+
+    if (selectedCard.isClicked) {
+      console.log('Стоп игра')
+    }
+    else {
+      setCards(prev => 
+                (prev.map(item => 
+                  item === selectedCard 
+                  ? {...item, isClicked: true} 
+                  : item)))
+        }
+    }
+
+    return (
+        <div className='cardsGrid'>
+        {
+            cards.map((item)=> (
+            <Card image={item.image} 
+                    id={item.id}
+                    key={item.id}
+                    onClick={handleCardClick}></Card>
+            ))   
+        }
+        </div>
+    )
+}
